02e4234013ecfa6f541af1a6c38a1f837c6ac85b
jcreeno77 <jcreeno77@gmail.com>
2023-06-18 16:55:25 -0500

    Added functionality for movement
    Sure the movement code could be better but it's what was intuited

--- Assets/OpenAITesting.cs
+++ Assets/OpenAITesting.cs
@@ -28,7 +28,10 @@ public class OpenAITesting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            test();
+        }
     }
 
     async void test()
@@ -42,12 +45,13 @@ public class OpenAITesting : MonoBehaviour
 
         var chatPrompts = new List<Message>
         {
-            new Message(Role.System,"You generate a JSON for the user. You do not add extra commentary or speech. You only give the JSON." +
-            "And example is this: \n" +
+            new Message(Role.System,"You generate a JSON for the user. You do not add extra commentary or speech. You only give the JSON.\n" +
+            "Never respond conversationally with something like: 'Sure! Here's a JSON for a character with a random Russian name:'\n" +
+            "An example is this: \n" +
             "User: Hey give me a JSON of a character named John. \n" +
             "Response: {\"Name\":\"John\", \"Age\":\"30\", \"Height\": \"5.8\"}\n" +
-            "You will ONLY include Name, Age, and Height."),
-            new Message(Role.User, "Hey give me a JSON for a character named Margaret.")
+            "You will ONLY include Name, Age, and Height. The age range should be between 5-60"),
+            new Message(Role.User, "Hey give me a JSON for a character with a random russian name.")
         };
 
         var chatRequest = new ChatRequest(chatPrompts, Model.GPT3_5_Turbo);
--- Assets/SphereMovement.cs
+++ Assets/SphereMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SphereMovement : MonoBehaviour
 {
     float moveSpeed;
+    float vSpeed;
+    float hSpeed;
     [SerializeField] GameObject OpenAIHandler;
     Vector3 movement;
     // Start is called before the first frame update
@@ -16,11 +18,53 @@ public class SphereMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //up key
         moveSpeed = OpenAIHandler.GetComponent<OpenAITesting>().movementSpeed;
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            movement = transform.forward * moveSpeed;
+            vSpeed += 1f;
+
+        }else if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            vSpeed -= 1f;
         }
+        //down key
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            vSpeed += -1f;
+
+        }
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            vSpeed -= -1f;
+        }
+        //right key
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            hSpeed += 1f;
+
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            hSpeed -= 1f;
+
+        }
+        //left key
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            hSpeed += -1f;
+
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            hSpeed -= -1f;
+        }
+        //assign movement
+        movement *= 0;
+        movement += transform.forward * vSpeed;
+        movement += transform.right * hSpeed;
+        movement = movement.normalized;
+        movement *= moveSpeed;
         GetComponent<Rigidbody>().velocity = movement;
     }
 }
